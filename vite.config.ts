ce452a5223fab5c1fe1ffe1462b5e78cfad7b3aa
stunter777 <102242794+stunter777@users.noreply.github.com>
2025-04-16 01:33:49 +0300

    add vercel json

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  ssr: {
-    noExternal: ["react-force-graph-2d"],
-  },
 });
